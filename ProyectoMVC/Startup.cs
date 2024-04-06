3e607bff3487cdf2af93f76c9dc6cd4a22800796
Usuario <Usuario@DESKTOP-4F7A3HE>
2024-04-06 14:09:48 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoMVC.Startup))]
+namespace ProyectoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
